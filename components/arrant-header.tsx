1f28f4d0bd9ec4a14f4d206b563141815233d726
swethareddyganta <swetha0937@gmail.com>
2025-07-14 09:00:09 -0500

    title change

--- components/arrant-header.tsx
+++ components/arrant-header.tsx
@@ -1,15 +1,9 @@
 export function ArrantHeader() {
   return (
     <div className="mb-8 text-center">
-      <h1 className="text-lg font-semibold text-gray-800">Arrant Technologies</h1>
+      <h1 className="text-lg font-semibold text-gray-800">STERI Clean Air - HVAC Matrix</h1>
       <p className="text-sm text-gray-500">
-        <a href="mailto:info@arranttech.com" className="hover:text-blue-600">
-          info@arranttech.com
-        </a>
-        <span className="mx-2 text-gray-300">|</span>
-        <a href="http://www.arranttech.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
-          www.arranttech.com
-        </a>
+        By Arrant Dynamics, a division of Arrant Technologies India, Pvt Ltd
       </p>
     </div>
   )
